2cf8738a44a177ce6c0e16f9d5ae568130d6da49
SerhiiL06 <142268536+SerhiiL06@users.noreply.github.com>
2024-05-31 17:47:48 +0300

    update dockerfiles

--- backend/tests/trackers/test_views.py
+++ backend/tests/trackers/test_views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import reverse
+
 from src.trackers.common import ClickAction, Interests
 from src.trackers.models import Campaign, Click, Lead, Offer
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 
+
+
+
 function App() {
   return (
     <div className="App">
-
+      <h1>Hello from word</h1>
     </div>
   );
 }
-
 export default App;
